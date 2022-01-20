527980ca5be30570aafad89674e01e33ecb5de20
ericwpei <97645828+ericwpei@users.noreply.github.com>
2022-01-19 17:39:10 -0800

    infinite loop

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,11 +12,13 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            System.out.println("Value of current index before loop: " + currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            System.out.println("Value of current index after loop: " + currentIndex);
         }
         return toReturn;
     }
