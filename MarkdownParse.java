e371e3f956f172eceb7fc34f2a8850cd3e076cee
ericwpei <97645828+ericwpei@users.noreply.github.com>
2022-01-27 20:11:25 -0800

    fixing markdownparse printing double

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -3,43 +3,42 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-         // find the next [, then find the ], then find the (, then take up to
-         // the next )
-         int currentIndex = 0;
-         int pastCloseParen = 0;
-         while(currentIndex < markdown.length()) {
-             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+        // find the next [, then find the ], then find the (, then take up to
+        // the next )
+        int currentIndex = 0;
+        int pastCloseParen = 0;
+        while(currentIndex < markdown.length()) {
+            int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            
+            // System.out.println("Value of current index before loop: " + currentIndex);
+            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            int openParen = markdown.indexOf("(", nextCloseBracket);
+            int closeParen = markdown.indexOf(")", openParen);
 
-             // System.out.println("Value of current index before loop: " + currentIndex);
-             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-             int openParen = markdown.indexOf("(", nextCloseBracket);
-             int closeParen = markdown.indexOf(")", openParen);
-             toReturn.add(markdown.substring(openParen + 1, closeParen));
-             currentIndex = closeParen + 1;
-
-             if(pastCloseParen == closeParen || nextOpenBracket == -1) {
-                 break;
-             }
-             pastCloseParen = closeParen;
-
-             // System.out.println("Index of next open bracket: " + nextOpenBracket);
-             // System.out.println("Index of next open bracket - 1: " + (nextOpenBracket - 1));
-             if(nextOpenBracket != 0) {
-                 if(!markdown.substring(nextOpenBracket-1, nextOpenBracket).equals("!")){
-                     toReturn.add(markdown.substring(openParen + 1, closeParen));
-                 }
-                 currentIndex = closeParen + 1;
-             } else {
-                 toReturn.add(markdown.substring(openParen + 1, closeParen));
-                 currentIndex = closeParen + 1;
-             }
-             // System.out.println("Value of current index after loop: " + currentIndex);
-         }
-         return toReturn;
-     }
+            if(pastCloseParen == closeParen || nextOpenBracket == -1) {
+                break;
+            }
+            pastCloseParen = closeParen;
+            
+            // System.out.println("Index of next open bracket: " + nextOpenBracket);
+            // System.out.println("Index of next open bracket - 1: " + (nextOpenBracket - 1));
+            if(nextOpenBracket != 0) {
+                if(!markdown.substring(nextOpenBracket-1, nextOpenBracket).equals("!")){
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                }
+                currentIndex = closeParen + 1;
+            } else {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+            // System.out.println("Value of current index after loop: " + currentIndex);
+        }
+        return toReturn;
+    }
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,64 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+
+
+//To compile/run:
+//javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+//java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks1() throws IOException{
+        ArrayList<String> linkTester = new ArrayList<>();
+        linkTester.add("https://something.com");
+        linkTester.add("some-page.html");
+
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(linkTester, links);
+    }
+
+    @Test
+    public void testGetLinks2() throws IOException{
+        ArrayList<String> linkTester = new ArrayList<>();
+        linkTester.add("https://something.com");
+        linkTester.add("https://something.com");
+
+        Path fileName = Path.of("test-file2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(linkTester, links);
+    }
+
+    @Test
+    public void testGetLinks3() throws IOException{
+        ArrayList<String> linkTester = new ArrayList<>();
+        linkTester.add("https://youtube.com");
+
+        Path fileName = Path.of("test-file3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(linkTester, links);
+    }
+
+    @Test
+    public void testGetLinks4() throws IOException{
+        ArrayList<String> linkTester = new ArrayList<>();
+        linkTester.add("https://random.com");
+
+        Path fileName = Path.of("test-file4.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(linkTester, links);
     }
 }
